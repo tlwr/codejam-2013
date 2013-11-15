ba4825dee746ab393d26f35764e57596e7d2f32a
Timothee Guerin <timothee.guerin@outlook.com>
2013-11-15 16:24:20 -0500

    siuhofe

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Codejam2013dreamtim::Application.config.secret_key_base = 'ed4cbc199d72d46ce7881179b0d4e11e6d342552f69839fc61acff7f9c7d3dc8de6d9f048f8a5af338bdbaba28c8b2f56cdfab0a7866588f0e29b293aa157c62'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Codejam2013dreamtim::Application.config.session_store :cookie_store, key: '_codejam2013dreamtim_session'
