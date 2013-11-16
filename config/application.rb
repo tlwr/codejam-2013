1097fb186b79e1378d6b77b50e707e39a5b61cec
Timothee Guerin <timothee.guerin@outlook.com>
2013-11-16 11:49:45 -0500

    more algo

--- config/application.rb
+++ config/application.rb
@@ -2,9 +2,6 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-Dir[Rails.root + 'lib/**/*.rb'].each do |file|
-  require file
-end
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
