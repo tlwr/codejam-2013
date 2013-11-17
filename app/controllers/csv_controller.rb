96c118cb719c55b9486ee47ffe925d7467ef06c3
Timothee Guerin <timothee.guerin@outlook.com>
2013-11-16 22:11:58 -0500

    more algo

--- app/controllers/csv_controller.rb
+++ app/controllers/csv_controller.rb
@@ -19,8 +19,9 @@ class CsvController < ApplicationController
 
   def local
     file = File.open('sample_input.csv')
-    matrix = Utils::Algorithm::fill_missing_values(Utils::Algorithm::csv_to_matrix(file.read))
-
+    matrix = (Utils::Algorithm::csv_to_matrix(file.read))
+    puts matrix
+    puts '==========================================='
     render :text => get_missing_value(matrix).join('<br/>'), :status => 200
   end
 
@@ -37,12 +38,24 @@ class CsvController < ApplicationController
       row = csv.row(i).to_a
       array << row
       if csv.row(i)[Utils::Csv::CONSUMPTION] == 0.0
-
-
+        tmp = nil
+        if array[array.size-1][Utils::Csv::RADIATION] != 0.0
+          tmp = array[i].clone
+          array[array.size-1][Utils::Csv::RADIATION] = 0.0
+          array[array.size-1][Utils::Csv::HUMIDITY] = 0.0
+          array[array.size-1][Utils::Csv::TEMPERATURE] = 0.0
+          array[array.size-1][Utils::Csv::WINDSPEED] = 0.0
+        end
         val = Utils::Algorithm.forcast_next_value(Matrix.rows(array), array.size-1)
         puts 'last: ' + array[array.size-2][Utils::Csv::CONSUMPTION].to_s
         puts val
-        row[Utils::Csv::CONSUMPTION]= val
+        array[array.size-1][Utils::Csv::CONSUMPTION] = val
+        unless tmp.nil?
+          array[array.size-1][Utils::Csv::RADIATION] = tmp[Utils::Csv::RADIATION]
+          array[array.size-1][Utils::Csv::HUMIDITY] = tmp[Utils::Csv::HUMIDITY]
+          array[array.size-1][Utils::Csv::TEMPERATURE] = tmp[Utils::Csv::TEMPERATURE]
+          array[array.size-1][Utils::Csv::WINDSPEED] = tmp[Utils::Csv::WINDSPEED]
+        end
         vals << val
       else
         #p = Point::from_row(csv.row(i))
